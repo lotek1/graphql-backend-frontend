c526d492f06f4dba7d9199a16922e3c429b3eaa7
Lotek <lotek@Loteks-MBP.home>
2022-04-09 19:28:17 +0200

    initial

---
+++ server/FakeData.js
@@ -0,0 +1,62 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "jonno",
+    age: "23",
+    nationality: "NORWAY",
+    friends: [
+      {
+        id: 2,
+        name: "Jeremy",
+        username: "jerrr",
+        age: "44",
+        nationality: "SWEDEN",
+      },
+      {
+        id: 3,
+        name: "Leila",
+        username: "Lollo",
+        age: "20",
+        nationality: "DENMARK",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Jeremy",
+    username: "jerrr",
+    age: "44",
+    nationality: "SWEDEN",
+  },
+  {
+    id: 3,
+    name: "Leila",
+    username: "Lollo",
+    age: "20",
+    nationality: "DENMARK",
+  },
+  {
+    id: 4,
+    name: "Susanne",
+    age: "23",
+    username: "Susi",
+    nationality: "GERMANY",
+  },
+  {
+    id: 5,
+    name: "Joker",
+    username: "Joker",
+    age: "29",
+    nationality: "ITALY",
+  },
+  {
+    id: 6,
+    name: "Brian",
+    username: "Brianno",
+    age: "32",
+    nationality: "PORTUGAL",
+  },
+];
+
+module.exports = { UserList };
